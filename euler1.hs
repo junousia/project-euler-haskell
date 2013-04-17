7061abe1366175a269cb4ffdaca8a43b420a35b2
Jukka Nousiainen <nousiaisenjukka@gmail.com>
2013-04-17 23:32:00 +0300

    modified the printout

--- euler1.hs
+++ euler1.hs
@@ -8,5 +8,7 @@ multiples (x:xs)
 	| (mod x 5) /= 0 && (mod x 3) /= 0 = [] ++ multiples xs
 	| otherwise = [x] ++ multiples xs
 
-main = printf "Sum of numbers divisible by 3 or 5 in the range of [1..1000]:\n%d" (sum (multiples [1..1000])::Int)
-
+main :: IO ()
+main = do
+	printf "Euler Project problem 1 - Multiples of 3 and 5:\n"
+	printf "%d\n" (sum (multiples [1..1000])::Int)
